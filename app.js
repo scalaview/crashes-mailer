9cb9e56dc1f95fc7f0ab5b24a4dccfaa458c4764
scalaview <chailink100@gmail.com>
2019-07-22 18:35:57 +0800

    var result

--- app.js
+++ app.js
@@ -46,13 +46,13 @@ function n_times_filter(crashelogs, body){
         twoHours = (2 * 60 * 60 * 1000),
         results = crashelogs.find({timestamp: { $gt: now - twoHours }});
 
-    if(results == null){
-        let result = { content: content, count: 0, timestamp: now }
+    if(results == null || results.length <= 0){
+        var result = { content: content, count: 0, timestamp: now }
         crashelogs.insert(result);
     }else{
         let findOne = false;
-        for(let i=0; i<result.length; i++){
-            let result = results[i]
+        for(let i=0; i<results.length; i++){
+            var result = results[i]
             if( natural.JaroWinklerDistance(result.content, content) > 0.6 ){ // consider tow content are same
                 result.count += 1
                 result.timestamp = now
@@ -62,7 +62,7 @@ function n_times_filter(crashelogs, body){
             }
         }
         if(!findOne){
-            let result = { content: content, count: 0, timestamp: (new Date()).getTime() }
+            var result = { content: content, count: 0, timestamp: (new Date()).getTime() }
             crashelogs.insert(result);
         }
     }
